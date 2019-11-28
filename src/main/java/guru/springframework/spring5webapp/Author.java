2432c49a5fa0d53c217bb919dab1250cf806ccb0
inv01 <gaiatoorion@gmail.com>
2019-11-28 13:46:49 +0100

    jpa example

--- src/main/java/guru/springframework/spring5webapp/Author.java
+++ src/main/java/guru/springframework/spring5webapp/Author.java
@@ -3,11 +3,12 @@ package guru.springframework.spring5webapp;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
+@Entity
 public class Author {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO) 
@@ -17,9 +18,11 @@ public class Author {
 	private String firstName;
 	private String lastName;
 	
-	@ManyToMany
+	@ManyToMany(mappedBy = "authors")	
 	private Set<Book> books = new HashSet<>();
 	
+	
+
 	public Author() {	
 	}
 	
@@ -35,6 +38,30 @@ public class Author {
 	
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return id == null ? 0 : id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null) return false;
+		} else 
+			if (!id.equals(other.id)) return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
 	}
 	
 	
--- src/main/java/guru/springframework/spring5webapp/Book.java
+++ src/main/java/guru/springframework/spring5webapp/Book.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import org.hibernate.annotations.ManyToAny;
@@ -22,6 +24,8 @@ public class Book {
 	private String publisher;
 	
 	@ManyToMany
+	@JoinTable(name="author_book", joinColumns = @JoinColumn(name = "book_id"),
+	inverseJoinColumns = @JoinColumn(name = "author_id"))
 	private Set<Author> authors = new HashSet<>();
 
 	public Book() {
@@ -38,6 +42,31 @@ public class Book {
 		this.title = title;
 		this.isbn = isbn;
 		this.publisher = publisher;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id == null ? 0 : id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null) return false;
+		} else 
+			if (!id.equals(other.id)) return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", publisher=" + publisher + ", authors="
+				+ authors + "]";
 	}
 	
 	
